c7a4ce6f8aa2910536c64261aca83f9c8a8cb881
Moitreya Chatterjee <metro-smiles@users.noreply.github.com>
2025-04-24 16:11:50 -0400

    Installation File

---
+++ Install.sh
@@ -0,0 +1,14 @@
+conda create -n prog_gen python=3.8
+conda activate prog_gen
+pip install dotmap
+pip install scipy
+pip install argparse
+pip install h5py
+pip install scikit-image
+pip install imageio-ffmpeg
+pip install matplotlib
+pip install opencv-python
+pip install Box2D
+pip install openai
+pip install dill
+pip install pygame
